09935d3b5d379aa92b96d4f6266f4e1a1b9074cb
ANZELINAHAREVICA-STU <G00414991@atu.ie>
2025-11-26 12:04:36 +0000

    Created Project

---
+++ src/test/java/ie/atu/bookshopproject/BookShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.bookshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
